dd3e290b1c14e60be5bbd4560b6cfd6011655dee
1396abdullah <142124258+1396abdullah@users.noreply.github.com>
2024-02-26 08:09:29 +0200

    some changes
    Some changes in c++ code

---
+++ files/src/files.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : files.cpp
+// Author      : Abdullah
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  // Create and open a text file
+  ofstream MyFile("filename.txt");
+
+  // Write to the file
+  MyFile << "Files can be tricky, but it is fun enough!";
+
+  // Close the file
+  MyFile.close();
+}
